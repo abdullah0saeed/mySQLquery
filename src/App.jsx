629682921bd8377db795989ee51eb1a06720f520
Abdullah Saeed <abdullahhamouda056@gmail.com>
2023-05-28 11:09:13 +0300

    1st

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+// import "./App.css";
+import Query from "./pages/Query.jsx";
+
+function App() {
+  return <Query />;
+}
+
+export default App;
---
+++ src/pages/Query.jsx
@@ -0,0 +1,110 @@
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// import "../css/buttons.css";
+import config from "../../config";
+
+export default function Query() {
+  const [query, setQuery] = useState("");
+  const [view, setView] = useState([]);
+
+  const sendQuery = async () => {
+    try {
+      const res = await fetch(`${config.serverUrl}/query`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ query }),
+      });
+
+      return await res.json();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const showView = (data) => {
+    let row = [];
+    let head = [];
+    for (let key in data[0]) {
+      head.push(
+        <label
+          className="border border-2 d-flex justify-content-center  bg-warning fs-6"
+          style={{ width: 230 }}
+        >
+          {key}
+        </label>
+      );
+    }
+    row.push(
+      <div className="d-flex justify-content-center flex-row">{head}</div>
+    );
+
+    data.map((el, i) => {
+      head = [];
+      for (let key in data[i]) {
+        head.push(
+          <textarea
+            className="border border-2 d-flex justify-content-center text-center "
+            style={{ width: 230 }}
+          >
+            {data[i][key]}
+          </textarea>
+        );
+      }
+      row.push(
+        <div className="d-flex justify-content-center flex-row">{head}</div>
+      );
+    });
+
+    setView(row);
+  };
+
+  return (
+    <div style={{ margin: 20, padding: 20 }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "start",
+        }}
+      >
+        <label style={{ fontSize: 28, margin: 3 }}>enter query</label>
+        <textarea
+          cols={80}
+          rows={3}
+          style={{ padding: 5, fontSize: 18 }}
+          value={query}
+          onChange={(e) => {
+            setQuery(e.target.value);
+          }}
+        />
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: 40,
+        }}
+      >
+        <button
+          type="submit"
+          className="btn btn-primary"
+          onClick={() => {
+            sendQuery().then((data) => {
+              showView(data);
+            });
+          }}
+        >
+          submit
+        </button>
+      </div>
+
+      <div className="overflow-auto mt-5">
+        <div className="d-flex justify-content-center flex-column">{view}</div>
+      </div>
+    </div>
+  );
+}
